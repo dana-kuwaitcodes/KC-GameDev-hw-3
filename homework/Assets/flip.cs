a7daaa36b5adfaad3c97d2c87d7482336a46ba40
dana-kuwaitcodes <danahossam056@gmail.com>
2022-08-27 18:04:45 +0300

    hw3
    حاولت ادور على شخصيه ثانيه بس كانوا بدون anmaition

---
+++ homework/Assets/flip.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flip : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    bool faceRight = true;
+
+    public GameObject bullet;
+    GameObject bulletClone;
+    public Transform leftSpawn;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipPlayer();
+        fire();
+    }
+    void FlipPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && faceRight == false)
+        {
+            sprite.flipX = false;
+            faceRight = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && faceRight == true)
+        {
+            sprite.flipX = true;
+            faceRight = false;
+        }
+    }
+    void fire()
+    {
+        if (Input.GetMouseButtonDown(0) && faceRight)
+        {
+            bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+            Destroy(bulletClone, 1.5f);
+        }
+        else if (Input.GetMouseButtonDown(0) && !faceRight)
+        {
+            bulletClone = Instantiate(bullet, leftSpawn.position, leftSpawn.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.right * -speed;
+            Destroy(bulletClone, 1.5f);
+
+        }
+    }
+}
